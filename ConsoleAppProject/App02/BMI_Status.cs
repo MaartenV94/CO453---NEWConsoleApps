1e91c1080e037e5981a30d6e5211acc49d6fd0bf
Maarten Vanderbeeken <91742234+MaartenV94@users.noreply.github.com>
2022-03-21 10:34:16 +0000

    NEW BMI_Status

---
+++ ConsoleAppProject/App02/BMI_Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleAppProject.App02
+{
+    /// <summary>
+    /// A listing of the WHO weight classifications.
+    /// </summary>
+    /// <author>
+    /// Maarten Vanderbeeken version 1.0
+    /// </author>
+    public enum BMI_Status
+    {
+       None,
+       Underwieght,
+       Normalweight,
+       Overwieght,
+       ObeseI,
+       ObeseII,
+       ObeseIII,
+    }
+}
